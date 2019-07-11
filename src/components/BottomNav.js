d2c0703bff707a6ca15eed8ba7f9b18f17cc52e5
Jesús Ferretti <jferrettiboke@gmail.com>
2019-07-11 17:08:57 +0200

    Add BottomNav

---
+++ src/components/BottomNav.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Box from "./Box";
+
+export function BottomNav({ sx, ...rest }) {
+  return (
+    <Box
+      as="nav"
+      sx={{
+        // Raw
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        borderTop: "1px solid",
+        // Theme
+        borderColor: "gray.200",
+        width: "full",
+        // Extend
+        ...sx
+      }}
+      {...rest}
+    />
+  );
+}
+
+export function BottomNavLink({ children, icon, sx, ...rest }) {
+  return (
+    <Box
+      as="a"
+      sx={{
+        // Raw
+        display: "flex",
+        justifyContent: "center",
+        // Theme
+        p: "3",
+        color: "black",
+        textDecoration: "none",
+        // Extend
+        ...sx
+      }}
+      {...rest}
+    >
+      <Box sx={{ textAlign: "center" }}>
+        <Box sx={{ color: "gray.700" }}>{icon}</Box>
+        <Box sx={{ fontSize: "xs" }}>{children}</Box>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/BottomNav.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+  FaRegPaperPlane,
+  FaRegHeart,
+  FaRegMap,
+  FaRegCommentAlt,
+  FaRegUserCircle
+} from "react-icons/fa";
+
+import { BottomNav, BottomNavLink } from "./BottomNav";
+
+storiesOf("Components/BottomNav", module).add("default", () => (
+  <BottomNav sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>
+    <BottomNavLink href="#" icon={<FaRegPaperPlane />}>
+      Explore
+    </BottomNavLink>
+    <BottomNavLink href="#" icon={<FaRegHeart />}>
+      Saved
+    </BottomNavLink>
+    <BottomNavLink href="#" icon={<FaRegMap />}>
+      Trips
+    </BottomNavLink>
+    <BottomNavLink href="#" icon={<FaRegCommentAlt />}>
+      Inbox
+    </BottomNavLink>
+    <BottomNavLink href="#" icon={<FaRegUserCircle />}>
+      Profile
+    </BottomNavLink>
+  </BottomNav>
+));
