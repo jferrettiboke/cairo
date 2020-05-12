51f3076ba503c19c46dd24d9e3f06d93740846d6
Jesús Ferretti <jferrettiboke@gmail.com>
2020-05-13 01:19:45 +0200

    make Checkbox icon as a component

--- packages/primitives/src/components/Checkbox.tsx
+++ packages/primitives/src/components/Checkbox.tsx
@@ -2,6 +2,39 @@ import React from "react";
 import { Box } from "./Box";
 // export { Checkbox, CheckboxProps } from "theme-ui";
 
+function Icon() {
+  return (
+    <Box
+      data-part-id="icon"
+      sx={{
+        boxSizing: "border-box",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        pointerEvents: "none",
+        opacity: "0",
+        color: "rgb(255, 255, 255)",
+      }}
+    >
+      <svg
+        width="15"
+        height="15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ display: "block" }}
+      >
+        <path
+          d="M11.467 3.72686C11.7559 3.91576 11.8369 4.30309 11.648 4.592L7.39803 11.092C7.29787 11.2452 7.1356 11.3468 6.95405 11.3699C6.77251 11.3931 6.58992 11.3355 6.4545 11.2124L3.7045 8.71243C3.44909 8.48024 3.43027 8.08496 3.66246 7.82954C3.89465 7.57413 4.28993 7.55531 4.54534 7.7875L6.75295 9.79442L10.6018 3.90793C10.7907 3.61903 11.178 3.53796 11.467 3.72686Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </Box>
+  );
+}
+
 export function Checkbox(props: any) {
   return (
     <Box
@@ -20,9 +53,9 @@ export function Checkbox(props: any) {
       }}
     >
       <Box
+        {...props}
         as="input"
         type="checkbox"
-        {...props}
         sx={{
           boxSizing: "border-box",
           WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
@@ -66,34 +99,7 @@ export function Checkbox(props: any) {
           borderImage: "initial",
         }}
       >
-        <Box
-          data-part-id="icon"
-          sx={{
-            boxSizing: "border-box",
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            pointerEvents: "none",
-            opacity: "0",
-            color: "rgb(255, 255, 255)",
-          }}
-        >
-          <svg
-            width="15"
-            height="15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ display: "block" }}
-          >
-            <path
-              d="M11.467 3.72686C11.7559 3.91576 11.8369 4.30309 11.648 4.592L7.39803 11.092C7.29787 11.2452 7.1356 11.3468 6.95405 11.3699C6.77251 11.3931 6.58992 11.3355 6.4545 11.2124L3.7045 8.71243C3.44909 8.48024 3.43027 8.08496 3.66246 7.82954C3.89465 7.57413 4.28993 7.55531 4.54534 7.7875L6.75295 9.79442L10.6018 3.90793C10.7907 3.61903 11.178 3.53796 11.467 3.72686Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </Box>
+        <Icon />
       </Box>
     </Box>
   );
