12d2e08732a916e685dfa917790338d09bc86c38
Jesús Ferretti <jferrettiboke@gmail.com>
2020-05-14 02:11:46 +0200

    primitives: add react-axe

--- packages/primitives/.storybook/preview.js
+++ packages/primitives/.storybook/preview.js
@@ -1,6 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { addDecorator } from "@storybook/react";
 import { CairoProvider, Box } from "../src";
+
+if (process.env.NODE_ENV !== "production") {
+  var axe = require("react-axe");
+  axe(React, ReactDOM, 1000);
+}
 
 addDecorator((story) => (
   <CairoProvider theme={{}}>
